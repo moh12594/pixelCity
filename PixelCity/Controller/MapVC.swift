f412db2fc1366e2f42cbee711f76c70b8cf445ef
Mohamed SADAT <moh12594@gmail.com>
2017-11-26 18:26:13 +0100

    initial commit

---
+++ PixelCity/Controller/MapVC.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PixelCity
+//
+//  Created by Mohamed SADAT on 26/11/2017.
+//  Copyright © 2017 Mohamed SADAT. All rights reserved.
+//
+
+import UIKit
+
+class MapVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
